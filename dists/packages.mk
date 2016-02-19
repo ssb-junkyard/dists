c1f23a5f6dd0cc7d012613f1bfb9871275379ac2
Charles Lehner <cel@celehner.com>
2016-02-19 18:23:49 -0500

    Fix downloading package

--- dists/packages.mk
+++ dists/packages.mk
@@ -1,1 +1,1 @@
-PKG += ssbc/patchwork-electron/releases/download/2.4.1/ssb-patchwork-electron_2.4.1_amd64.deb
+PKG += ssbc/patchwork-electron/releases/download/v2.4.1/ssb-patchwork-electron_2.4.1_amd64.deb
