94e4ea203284be967d48b2c206b7323d57007a53
Charles Lehner <cel@celehner.com>
2016-02-22 23:36:32 -0500

    ssb-patchwork-electron v2.5.0

--- packages.mk
+++ packages.mk
@@ -1,1 +1,1 @@
-PKG_AMD64 += packages/patchwork-electron/releases/download/v2.4.1/ssb-patchwork-electron_2.4.1_amd64.deb
+PKG_AMD64 += packages/patchwork-electron/releases/download/v2.5.0/ssb-patchwork-electron_2.5.0_amd64.deb
